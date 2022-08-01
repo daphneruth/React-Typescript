4ec4cccc1698f222ff597116513d89743918f591
Daphne <daphneruth304@gmail.com>
2022-08-01 12:47:35 +0300

    rendering element

--- src/Components/Todos.tsx
+++ src/Components/Todos.tsx
@@ -1,4 +1,11 @@
 function Todos() {
-
+  return (
+    <div>
+        <ul>
+            <li>Learn Node</li>
+            <li>Learn React</li>
+        </ul>
+    </div>
+  )
 }
 export default Todos;
