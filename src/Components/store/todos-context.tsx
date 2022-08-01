6b5231c1b29567ca0b9537e76bb9a04cd804942e
Daphne <daphneruth304@gmail.com>
2022-08-01 23:42:35 +0300

    updated

--- src/Components/store/todos-context.tsx
+++ src/Components/store/todos-context.tsx
@@ -1,32 +1,47 @@
+import React, { useState } from 'react';
 
-import React, { useState } from "react";
-import Todo from "../../Models/todo";
+import Todo from '../../Models/todo';
 
-const TodoContext = React.createContext<{
-    items: Todo[];
-    addTodo:()=> void;
-    removeTodo:(id:string)=>void;
+type TodosContextObj = {
+  items: Todo[];
+  addTodo: (text: string) => void;
+  removeTodo: (id: string) => void;
+};
 
-}>({
-    items: [],
-    addTodo: () => {},
-    removeTodo: (id:string)=> {}
-})
-const TodosContextProvider:React.FC= (props)=> {
-    useState() 
+export const TodosContext = React.createContext<TodosContextObj>({
+  items: [],
+  addTodo: () => {},
+  removeTodo: (id: string) => {},
+});
 
-    const removeTodoHandler = (todoId: string) => {
-        setTodos((prevTodos) => {
-          return prevTodos.filter((todo) => todo.id !== todoId);
-        });
-      };
+const TodosContextProvider: React.FC = (props) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-    const contextValue: TodosContextObj = {
-        items: todos,
-        addTodo: addTodoHandler,
-        removeTodo: removeTodoHandler,
-      }
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
 
-    return <TodosContext.Provider>{props.children}</TodosContext.Provider>
-}
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
 
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
+  const contextValue: TodosContextObj = {
+    items: todos,
+    addTodo: addTodoHandler,
+    removeTodo: removeTodoHandler,
+  };
+
+  return (
+    <TodosContext.Provider value={contextValue}>
+      {props.children}
+    </TodosContext.Provider>
+  );
+};
+
+export default TodosContextProvider;
