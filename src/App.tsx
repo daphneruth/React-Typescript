9d0b00776fa9cb4d9a0d96b65c62c7770d9d1be1
Daphne <daphneruth304@gmail.com>
2022-08-01 21:02:58 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import NewTodo from './Components/NewTodo';
-import Todos from './Components/Todos';
-import Todo from './Models/todo';
+import NewTodo from "./Components/NewTodo";
+import Todos from "./Components/Todos";
+import Todo from "./Models/todo";
 
 function App() {
-
-  const todos =[
-    new Todo('Learn Node'),
-    new Todo('Learn React')
-  ];
-  const addTodoHandler =(todoText:string)=> {
-
-  }
+  const todos = [new Todo("Learn Node"), new Todo("Learn React")];
+  const addTodoHandler = (todoText: string) => {};
   return (
     <div>
-    <NewTodo onAddTodo={addTodoHandler}/>
-    <Todos items={todos}/>  
-
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} />
     </div>
-    
   );
 }
 
