e3401c397e05dea9b11812d45b3a48ac41e9e2b6
Daphne <daphneruth304@gmail.com>
2022-08-01 14:00:25 +0300

    form submission

--- src/Components/NewTodo.tsx
+++ src/Components/NewTodo.tsx
@@ -1,2 +1,10 @@
-const NewTodo =()=>{}
+const NewTodo =()=>{
+    return(
+        <form>
+            <label>Todo text</label>
+            <input type='text'/>
+            <button>Add Todo</button>
+        </form>
+    )
+}
 export default NewTodo;
