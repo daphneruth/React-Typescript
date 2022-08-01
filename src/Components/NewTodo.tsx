82485b1d69ed85ae37fd2e47f656a791f18e7660
Daphne <daphneruth304@gmail.com>
2022-08-01 14:04:58 +0300

    react form Events

--- src/Components/NewTodo.tsx
+++ src/Components/NewTodo.tsx
@@ -1,4 +1,9 @@
+import React from "react";
+
 const NewTodo =()=>{
+const submitHandler =(event:React.FormEvent) =>{
+    event.preventDefault();
+}
     return(
         <form>
             <label htmlFor="text">Todo text</label>
