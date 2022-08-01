5d608f92eacee6ae37ea54169b5d7cf57e63f694
Daphne <daphneruth304@gmail.com>
2022-08-01 14:40:57 +0300

    fixed some bug

--- src/Components/NewTodo.tsx
+++ src/Components/NewTodo.tsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import React from "react";
 
 const NewTodo =()=>{
-    const todoInputRef =useRef<HTMLInputElement>;
+    const todoInputRef =useRef<HTMLInputElement>(null);
 const submitHandler =(event:React.FormEvent) =>{
     event.preventDefault();
 };
@@ -10,7 +10,7 @@ const submitHandler =(event:React.FormEvent) =>{
         <form onSubmit={submitHandler}>
             <label htmlFor="text">Todo text</label>
             <input type='text' id="text" ref={todoInputRef}/>
-            />
+            
             <button>Add Todo</button>
         </form>
     )
