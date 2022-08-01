1663db9243cda0ecf5e2b4278f6810233f48c918
Daphne <daphneruth304@gmail.com>
2022-08-01 23:17:53 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import { useState } from "react";
-import NewTodo from "./Components/NewTodo";
-import Todos from "./Components/Todos";
-import Todo from "./Models/todo";
+import { useState } from 'react';
+
+import NewTodo from './Components/NewTodo';
+import Todos from './Components/Todos';
+import Todo from './Models/todo';
 
 function App() {
-  const [todos, setTodo ]= useState<Todo[]>([]);
- 
-  const addTodoHandler = (todoText: string) => {;
-  const newTodo = new Todo(todoText);
-  setTodo((prevTodo) => {
-    return prevTodo.concat(newTodo)
-  })
-  }
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    });
+  };
+
   return (
     <div>
       <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
     </div>
   );
-  }
+}
+
 export default App;
--- src/Components/Todos.tsx
+++ src/Components/Todos.tsx
@@ -1,17 +1,23 @@
-import 'Todos.module.css';
-import React from "react";
-import TodoItem from "./TodoItem";
-import Todo from "../Models/todo";
+import React from 'react';
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+import TodoItem from './TodoItem';
+import Todo from '../Models/todo';
+import classes from './Todos.module.css';
+
+const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
+  props
+) => {
   return (
-    <div>
-      <ul>
-        {props.items.map((item) => (
-          <TodoItem key={item.id} text={item.text}/>
-        ))}
-      </ul>
-    </div>
+    <ul className={classes.todos}>
+      {props.items.map((item) => (
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+        />
+      ))}
+    </ul>
   );
 };
+
 export default Todos;
