3a0b2743764e0ebf0c46d01dae0f67594f7965a0
Daphne <daphneruth304@gmail.com>
2022-08-01 23:34:19 +0300

    using functional components

--- src/Components/store/todos-context.tsx
+++ src/Components/store/todos-context.tsx
@@ -11,4 +11,7 @@ const TodoContext = React.createContext<{
     addTodo: () => {},
     removeTodo: (id:string)=> {}
 })
+const TodosContextProvider:React.FC= (props)=> {
+    return <TodosContext.Provider>{props.children}</TodosContext.Provider>
+}
 
