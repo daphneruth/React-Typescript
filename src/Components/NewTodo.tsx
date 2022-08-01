c54bc50945142cd20e841912643453b431ca4abc
Daphne <daphneruth304@gmail.com>
2022-08-01 14:50:27 +0300

    updated ref and useRefs

--- src/Components/NewTodo.tsx
+++ src/Components/NewTodo.tsx
@@ -7,7 +7,11 @@ const submitHandler =(event:React.FormEvent) =>{
     event.preventDefault();
 
     const enteredText = todoInputRef.current?.value;
-};
+
+    if(enteredText?.trim().length ===0)
+{
+    return
+}};
     return(
         <form onSubmit={submitHandler}>
             <label htmlFor="text">Todo text</label>
