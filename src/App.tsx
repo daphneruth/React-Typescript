0fe9d1029bb838cd21802baf5afeb91d3d0a06fa
Daphne <daphneruth304@gmail.com>
2022-08-01 21:23:23 +0300

    updating state

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,18 @@ import Todo from "./Models/todo";
 
 function App() {
   const [todos, setTodo ]= useState<Todo[]>([]);
-  const addTodoHandler = (todoText: string) => {};
+ 
+  const addTodoHandler = (todoText: string) => {;
+  const newTodo = new Todo(todoText);
+  setTodo((prevTodo) => {
+    return prevTodo.concat(newTodo)
+  })
+  }
   return (
     <div>
       <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
-}
-
+  }
 export default App;
